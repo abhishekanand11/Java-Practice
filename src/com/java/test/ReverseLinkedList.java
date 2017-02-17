a565a6d61edb48e4bb6299b4c93b2cc58fa602a8
abhishekanand11 <abhishek.anand@freecharge.com>
2017-02-18 02:34:37 +0530

    Reverse a stack without using another stack or queue or extra space

--- src/com/java/test/ReverseLinkedList.java
+++ src/com/java/test/ReverseLinkedList.java
@@ -1,34 +1,37 @@
 package com.java.test;
 
+/**
+ * This class is used to reverse a linked list
+ * 
+ * @author abhishek anand
+ *
+ */
 public class ReverseLinkedList {
-	/* Return reference of new head of the reverse linked list */
-	 class Node {
-	     int value;
-	    Node next;
-	    Node(int value) {
-	        this.value = value;
-	    }
-	} 
+	class Node {
+		int value;
+		Node next;
 
-	
-	    // This function should reverse linked list and return
-	   // head of the modified linked list.
-	   Node reverse(Node head)
-	   {
-	        // add code here
-	        Node prev = null;
-	        Node curr = head;
-	        Node next = null;
-	        
-	        while(curr != null){
-	            next = curr.next;
-	            curr.next = prev;
-	            prev = curr;
-	            curr = next;
-	        }
-	        head = prev;
-	        return head;
-	   }
-	
+		Node(int value) {
+			this.value = value;
+		}
+	}
+
+	// This function should reverse linked list and return
+	// head of the modified linked list.
+	Node reverse(Node head) {
+		// add code here
+		Node prev = null;
+		Node curr = head;
+		Node next = null;
+
+		while (curr != null) {
+			next = curr.next;
+			curr.next = prev;
+			prev = curr;
+			curr = next;
+		}
+		head = prev;
+		return head;
+	}
 
 }
