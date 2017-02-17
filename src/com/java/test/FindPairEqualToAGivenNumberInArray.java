440cfbeda32fdaefc3bc04c1aa97005cb459c5ff
abhishekanand11 <abhishek.anand@freecharge.com>
2017-02-18 01:13:07 +0530

    adding comments

--- src/com/java/test/FindPairEqualToAGivenNumberInArray.java
+++ src/com/java/test/FindPairEqualToAGivenNumberInArray.java
@@ -3,23 +3,30 @@ package com.java.test;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * This class is used to find the pair of numbers in an array whose sum is equal to the
+ * given number
+ * 
+ * @author abhishek anand
+ *
+ */
 public class FindPairEqualToAGivenNumberInArray {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-        int A[] = {-1, 4, 45, 6, 10, 1};
-        int n = 16;
-        findPairEqualToGivenSum(A, 0);
+		int A[] = { -1, 4, 45, 6, 10, 1 };
+		int n = 16;
+		findPairEqualToGivenSum(A, 0);
 
 	}
-	
-	public static void findPairEqualToGivenSum(int [] arr, int sum){
+
+	public static void findPairEqualToGivenSum(int[] arr, int sum) {
 		Map<Integer, Boolean> pairMap = new HashMap<Integer, Boolean>();
-		
-		for(int i = 0; i < arr.length; i++){
+
+		for (int i = 0; i < arr.length; i++) {
 			int temp = sum - arr[i];
-			if(pairMap.containsKey(temp)){
+			if (pairMap.containsKey(temp)) {
 				System.out.println("The pair equal to given sum [" + sum + "] is : " + temp + " and " + arr[i]);
 			} else {
 				pairMap.put(arr[i], Boolean.TRUE);
